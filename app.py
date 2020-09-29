9af8964528edca2b137fad0d77589146acc8fd2c
baophuc27 <phucphucbao2710@gmail.com>
2020-09-29 16:08:41 +0700

    fix connect port

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def upload_file():
       return res
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
+  app.run(host='0.0.0.0',port=6001,debug=True,threaded=True)
